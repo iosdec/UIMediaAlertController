2c6663690216f2b5ca4c8068bf96aa4886994497
Declan Land <thejailbreaker@me.com>
2018-03-14 13:36:27 +0100

    Version 2.0
    Improved system, completion handlers and UIMedia objects.

---
+++ UIMedia.m
@@ -0,0 +1,13 @@
+//
+//  UIMedia.m
+//  iOSDec
+//
+//  Created by iOSDec on 09/11/2017.
+//  Copyright © 2017 iosdec. All rights reserved.
+//
+
+#import "UIMedia.h"
+
+@implementation UIMedia
+
+@end
